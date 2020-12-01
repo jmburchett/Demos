a98598f828facbf5d0b3b4868952cfdcd138dd8a
jmburchett <71105332+jmburchett@users.noreply.github.com>
2020-12-01 12:30:09 -0500

    Add files via upload

---
+++ TMPaint/Paint/TMPaint.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Paint
+{
+    partial class frmTMPaint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlDraw = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlDraw
+            // 
+            this.pnlDraw.BackColor = System.Drawing.Color.White;
+            this.pnlDraw.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pnlDraw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDraw.Location = new System.Drawing.Point(0, 0);
+            this.pnlDraw.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlDraw.Name = "pnlDraw";
+            this.pnlDraw.Size = new System.Drawing.Size(784, 561);
+            this.pnlDraw.TabIndex = 0;
+            this.pnlDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlDraw_MouseDown);
+            this.pnlDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlDraw_MouseMove);
+            this.pnlDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlDraw_MouseUp);
+            // 
+            // frmTMPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pnlDraw);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "frmTMPaint";
+            this.Text = "TMPaint";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmPaint_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlDraw;
+    }
+}
+
---
+++ TMPaint/Paint/TMPaint.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class frmTMPaint : Form
+    {
+        Graphics gfx;
+        Pen p = new Pen(Color.Black);
+        SolidBrush sb = new SolidBrush(Color.Black);
+        bool canPaint = false;
+
+        public frmTMPaint()
+        {
+            InitializeComponent();
+            gfx = pnlDraw.CreateGraphics();
+            gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+
+        private void pnlDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (canPaint)
+            {
+                gfx.FillEllipse(sb, e.X - 2, e.Y - 2, 8, 8);
+            }
+        }
+
+        private void pnlDraw_MouseDown(object sender, MouseEventArgs e)
+        {
+            canPaint = true;
+            gfx.FillEllipse(sb, e.X - 2, e.Y - 2, 8, 8);
+        }
+
+        private void pnlDraw_MouseUp(object sender, MouseEventArgs e)
+        {
+            canPaint = false;
+        }
+
+        private void frmPaint_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                pnlDraw.Invalidate();
+            }
+
+            switch (e.KeyCode)
+            {
+                case Keys.Z:
+                    sb.Color = Color.Red;
+                    break;
+                case Keys.X:
+                    sb.Color = Color.Green;
+                    break;
+                case Keys.C:
+                    sb.Color = Color.Blue;
+                    break;
+                default:
+                    sb.Color = Color.Black;
+                    break;
+            }
+        }
+    }
+}
